e91d4afc1cc2f196b7dea8f658ccb5291b15daf0
reebok <reebok@localhost>
2015-05-14 20:05:30 +0300

    Добавил пропущенные файлы в предыдущий коммит

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,24 @@ require 'rails_helper'
 describe "User pages:" do
 	subject { page }
 
+	describe "index:" do
+		before do
+			sign_in FactoryGirl.create(:user)
+			FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+			FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+			visit users_path
+		end
+
+		it { should have_title('All users') }
+		it { should have_content('All users') }
+
+		it "should list each user" do
+			User.all.each do |user|
+				expect(page).to have_selector('li', text: user.name)
+			end
+		end
+	end
+
 	describe "profile page:" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
@@ -47,11 +65,10 @@ describe "User pages:" do
 			describe "after saving the user:" do
 				before { click_button submit }
  				let(:user) { User.find_by(email: 'example@railstutorial.org') }
-# 				let(:user) { FactoryGirl.create(:user) }
 #	не работает
 # 				it { should have_title(user.name) }
 				it { should have_selector('div.alert.alert-success', text: 'Welcome to') }
-				it { should have_link('Sign out') }
+				it { should have_link('Sign out', href: signout_path) }
 			end
 		end
 
@@ -67,7 +84,46 @@ describe "User pages:" do
 # 			before { click_link "Sign out" }
 # 			it { should have_link('Sign in') }
 # 		end
+	end
+
+
+	describe "edit profile user:" do
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit edit_user_path(user) }
+
+		describe "page:" do
+			it { should have_content("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href: 'http://gravatar.com/emails') }
+		end
+
+		describe "with invalid information:" do
+			before { click_button "Save changes" }
+			it { should have_content('error') }
+		end
+
+
+		describe "with valid information:" do
+			let(:new_name)  { "New Name" }
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Name", with: new_name
+				fill_in "E-mail",           with: new_email
+				fill_in "Password",         with: user.password
+				fill_in "Confirm Password", with: user.password
+				click_button "Save changes"
+			end
+
+			it { should have_title(new_name) }
+			it { should have_selector('div.alert.alert-success') }
+# do not works
+# 			it { should have_link('Sign out', href: signout_path) }
+			specify { expect(user.reload.name).to  eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
+		end
+
 
 	end
+
 
 end
