5d6f7aeca4cca3a508e803ea8eaab2ff2245bb33
reebok <reebok@localhost>
2015-07-16 13:52:12 +0300

    Remove '+ ok' from helper `microposts_helper.rb'

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -10,7 +10,7 @@ private
     def wrap_long_string(text, max_width = 30)
       zero_width_space = "&#8203;"
       regex = /.{1,#{max_width}}/
-      (text.length < max_width) ? text+'-ok' :
+      (text.length < max_width) ? text :
                                   text.scan(regex).join(zero_width_space)
     end
 end
