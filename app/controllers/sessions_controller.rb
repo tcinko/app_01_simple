11eda45050ed44d51dcf373db2693074880c1efb
reebok <reebok@localhost>
2015-09-01 16:27:56 +0300

    add note for guest login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,24 @@ class SessionsController < ApplicationController
   def create
 
 # variant whith `form_tag` helper in /app/views/session/new.html.erb
-# 	user = User.find_by_email(params[:email].downcase)
-# 	if user && user.authenticate(params[:password])
-#	........
+#   user = User.find_by_email(params[:email].downcase)
+#   if user && user.authenticate(params[:password])
+#   ........
 
 # variant whith `form_for` helper
-	user = User.find_by(email: params[:session][:email].downcase)
-	if user && user.authenticate(params[:session][:password])
-		sign_in user
-		redirect_to user
-	else
-	    flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
-            render 'new'
-	end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
 
 
   def destroy
-	 sign_out
+     sign_out
      redirect_to root_url
   end
 
